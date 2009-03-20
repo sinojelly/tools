8c376fdff5e41acdaaa281b1a412a4821fbc8105
c61764 <c61764@1ac62aac-115d-034e-982f-03f2283fc6de>
2009-03-20 21:14:53 +0000

    右键菜单支持路径有空格。
    git-svn-id: http://jelly-pc2:8081/svn/SmallProject/trunk/sendmail@21 1ac62aac-115d-034e-982f-03f2283fc6de

--- sendfile.bat
+++ sendfile.bat
@@ -1,7 +1,7 @@
-@rem %1 - 附件名称
+@rem %1 - 附件名称, 如果带有空格传入的时候需要加引号，这里不能统一加了，否则右键菜单运行也不能支持路径有空格
 @rem 发送附件时设置超时时间为600s
 
-blat -body "参见附件" -to chenguodong@huawei.com -ti 600 -base64 -charset Gb2312 -subject "%1" -attach  %1 -server smtp.163.com -f sinojelly@163.com -u sinojelly -pw cedar8786
+blat -body "参见附件" -to chenguodong@huawei.com -ti 600 -base64 -charset Gb2312 -subject %1 -attach  %1 -server smtp.163.com -f sinojelly@163.com -u sinojelly -pw cedar8786
 
 
 
