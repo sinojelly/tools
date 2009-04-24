44410d83891ac92e5513d00541b0cd818510183a
c61764 <c61764@1ac62aac-115d-034e-982f-03f2283fc6de>
2009-04-24 16:35:59 +0000

    增加简单修改批处理改变收件人的功能。
    git-svn-id: http://jelly-pc2:8081/svn/SmallProject/trunk/sendmail@43 1ac62aac-115d-034e-982f-03f2283fc6de

--- sendfile.bat
+++ sendfile.bat
@@ -1,7 +1,10 @@
 @rem %1 - 附件名称, 如果带有空格传入的时候需要加引号，这里不能统一加了，否则右键菜单运行也不能支持路径有空格
 @rem 发送附件时设置超时时间为600s
 
-blat -body "参见附件" -to chenguodong@huawei.com -ti 600 -base64 -charset Gb2312 -subject %1 -attach  %1 -server smtp.163.com -f sinojelly@163.com -u sinojelly -pw cedar8786
+set dst_emails=chenguodong@huawei.com
+@rem set dst_emails=sinojelly@163.com,mj.xiang@gmail.com,83711220@qq.com
+
+blat -body "参见附件" -to %dst_emails%  -ti 600 -base64 -charset Gb2312 -subject %1 -attach  %1 -server smtp.163.com -f sinojelly@163.com -u sinojelly -pw cedar8786
 
 
 
