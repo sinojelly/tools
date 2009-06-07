b2f82f20e177a9a252b766fe1712ca3e67883670
c61764 <c61764@1ac62aac-115d-034e-982f-03f2283fc6de>
2009-06-07 15:29:13 +0000

    解决了文件名含空格无法发送到问题。
    git-svn-id: http://jelly-pc2:8081/svn/SmallProject/trunk/sendmail@48 1ac62aac-115d-034e-982f-03f2283fc6de

--- Send.bat
+++ Send.bat
@@ -2,6 +2,6 @@ rem 注意: 文件名不能有空格，而且%%c不能改为"%%c"
 
 rem if exist send_log.txt del send_log.txt
 
-for %%c in (*.rar *.7z *.zip) do call sendfile.bat %%c
+for %%c in (*.rar *.7z *.zip) do call sendfile.bat "%%c"
 
 pause
