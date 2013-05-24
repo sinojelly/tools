399071656081c61ae20792a2a6e244b1e1b10ce1
sinojelly <sinojelly@gmail.com>
2013-05-24 21:17:23 +0800

    modify setEnv for guodong

--- source-insight/Macro/setEnv.bat
+++ source-insight/Macro/setEnv.bat
@@ -4,19 +4,19 @@
 setx SI_USER        chenguodong
 
 @rem 设置SourceInsight生成注释中的Copyright
-setx SI_COPYRIGHT   "Huawei Technologies Co., Ltd. 2009-2019. All rights reserved."
+setx SI_COPYRIGHT   "Guodong Workshop. 2009-2019. All rights reserved."
 
 @rem 设置TortoiseSVN路径
-setx TortoiseSVN_PATH  "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe"
+rem setx TortoiseSVN_PATH  "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe"
 
 @rem 设置SourceMonitor路径
-setx SRCMONITOR_PATH   "%GD_TOOL%\SourceMonitor\SourceMonitor.exe"
+rem setx SRCMONITOR_PATH   "%GD_TOOL%\SourceMonitor\SourceMonitor.exe"
 
 @rem 快捷键设置
 set HOT_KEY_CTRL=0
 set HOT_KEY_ALT=1
 @rem Shift最好用来扩展类似命令，不在这里设置
-@rem set HOT_KEY_SHIFT=0   
+set HOT_KEY_SHIFT=0
 
 @rem 如果快捷键是否全部替换
 set ALL_REPLACE=1
@@ -24,9 +24,8 @@ set ALL_REPLACE=1
 set SI_BASE_PRJ=D:\Users\Jelly\Documents\Source Insight\Projects\Base\Base.PR
 set SI_EM_FILE=guodong.em
 
-rem copy %SI_EM_FILE% "%SI_BASE_PRJ:Base.PR=%"
+copy %SI_EM_FILE% "%SI_BASE_PRJ:Base.PR=%"
 
-call "C:\Program Files\Source Insight 3\insight3.exe" -c GD_setup
-
+call "C:\Program Files (x86)\Source Insight 3\Insight3.exe" -c GD_setup
 
 pause
